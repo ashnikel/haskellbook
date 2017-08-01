8e65a89ef522b07d7d314cb8a8862f8bacf33d9a
ashNikel <ashnikel@gmail.com>
2017-08-01 12:13:16 +0300

    Chapter 9.11

---
+++ ch09/ch09.11_ex.hs
@@ -0,0 +1,15 @@
+-- 1
+zip' :: [a] -> [b] -> [(a, b)]
+zip' [] _ = []
+zip' _ [] = []
+zip' (x:xs) (y:ys) = (x, y) : zip' xs ys
+
+-- 2
+zipWith' :: (a -> b -> c) -> [a] -> [b] -> [c]
+zipWith' _ [] _ = []
+zipWith' _ _ [] = []
+zipWith' f (x:xs) (y:ys) = f x y : zipWith' f xs ys
+
+-- 3
+zip'' :: [a] -> [b] -> [(a, b)]
+zip'' = zipWith' (,)
